9edb76759e1d323a539660bbe750b214f5d41194
devansh05 <devansh2393@gmail.com>
2026-08-09 01:21:02 +0530

    add tools for agents

--- agents/open-ai-agents.js
+++ agents/open-ai-agents.js
@@ -1,20 +1,18 @@
 import { Agent, run, tool } from "@openai/agents";
 import { z } from "zod";
 import "dotenv/config";
-import axios from "axios";
+import { getWeatherData } from "../utilities/utilities.js";
 
 // const client = new OpenAI({
 //   apiKey: process.env.OPENAI_API_KEY,
 // });
 
-const getWeather = tool({
+const getWeatherTool = tool({
   name: "get_weather",
   description: "Return the weather for a given city.",
   parameters: z.object({ city: z.string() }),
   async execute({ city }) {
-    const url = `https://wttr.in/${encodeURIComponent(city)}?format=%C+%t`;
-    const response = await axios.get(url, { responseType: "text" });
-    return response.data;
+    return await getWeatherData(city);
   },
 });
 
@@ -24,7 +22,7 @@ async function init(prompt) {
     instructions:
       "Send me the output you received from tool directly wihtout any manipulation.",
     model: "gpt-5.6-luna",
-    tools: [getWeather],
+    tools: [getWeatherTool],
     // apiKey: process.env.OPENAI_API_KEY,
   });
 
